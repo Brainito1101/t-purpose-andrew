e20ea7009f363bc37ba9bd15edde3f3423f5860e
Sandeep Yadav <sandeepyaduvanshi28@gmail.com>
2026-01-05 17:39:51 +0530

    initial commit

--- app/api/lock/logout/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-const LOCK_COOKIE = 'site_lock'
-
-export async function POST() {
-  const res = NextResponse.json({ ok: true })
-  res.cookies.set(LOCK_COOKIE, '', { path: '/', maxAge: 0 })
-  return res
-}
