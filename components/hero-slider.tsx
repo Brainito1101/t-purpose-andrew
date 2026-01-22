fa1d506b6ac6611d5c445c7f223085f1042a7aab
Nidhi Mevada <accounts@brainito.com>
2026-01-22 18:04:16 +0530

    Update hero-slider.tsx

--- components/hero-slider.tsx
+++ components/hero-slider.tsx
@@ -161,7 +161,7 @@ export default function HeroSlider() {
                   <img
                     src="https://storage.googleapis.com/msgsndr/wcFeePbK2OSXkafPwdjz/media/697207cfeb392b8f5f82522d.png"
                     alt="Gratimystic Book"
-                    className="w-40 sm:w-48 h-auto drop-shadow-2xl opacity-90"
+                    className="w-48 sm:w-56 h-auto drop-shadow-2xl opacity-90"
                   />
 
                 </div>
