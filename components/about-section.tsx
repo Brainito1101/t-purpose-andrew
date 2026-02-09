efdd5a0144d6dc2a4ff71be4ac04c55322becd8e
Nidhi Mevada <accounts@brainito.com>
2026-02-09 13:21:41 +0530

    Update about-section.tsx

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -12,7 +12,7 @@ export default function AboutSection() {
           {/* <div className="relative w-full max-w-[350px] sm:max-w-sm mx-auto h-[240px] sm:h-[280px] md:h-[320px] lg:h-[400px]"> */}
            <div className="relative w-full max-w-[700px] sm:max-w-[750px] mx-auto h-[500px] sm:h-[600px] md:h-[700px] lg:h-[800px] flex justify-start">
   <Image
-    src="https://storage.googleapis.com/msgsndr/wcFeePbK2OSXkafPwdjz/media/695f9f0f7cc1b9c8df62522a.png"
+    src="https://storage.googleapis.com/msgsndr/wcFeePbK2OSXkafPwdjz/media/6987030fd017c3da9606ae0b.jpg"
     fill
     className="object-contain ml-[-20px]"
     priority
